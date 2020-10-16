c5205f9cbd48d591fbf0c07d9b9be74330e3cdab
Diija <pietrogds@hotmail.com>
2020-10-16 00:40:12 -0300

    Server.js routes fixed

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-//Importa as dependências que acabamos de instalar
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/front-sistema-de-judoca'));
+app.use(express.static('./dist/front-sistema-de-judocas'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/front-sistema-de-judoca/index.html'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/front-sistema-de-judocas/'});
 });
 
-// Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
